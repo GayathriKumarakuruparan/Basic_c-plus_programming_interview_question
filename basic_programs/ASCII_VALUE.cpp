e7d658e1ea21cf3fd8f7cff9cd8125f1a3fc43f2
GAYATHRI KUMARA KURUPARAN <85390065+GayathriKumarakuruparan@users.noreply.github.com>
2024-04-09 00:26:36 +0530

    Create ASCII_VALUE.cpp

---
+++ basic_programs/ASCII_VALUE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int
+main ()
+{
+  char ch;
+  cout << "Enter the char\n";
+  cin >> ch;
+  cout<<"The ASCII value of "<<ch<<" is "<<int(ch)<<endl;
+  return 0;
+}
